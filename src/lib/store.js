19a3ba43ec2d4577e96f2abc522ae11a74698111
f6bfb5 <f6bfb5@gmail.com>
2022-08-04 21:58:09 +0800

    fix api url

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,4 +2,4 @@ import { readable } from 'svelte/store';
 
 export const twitchApiUrl = readable('https://api.twitch.tv/helix');
 export const twitchApiClientId = readable('9tg4wj9aa662dtxh3im93jkwu516bs0');
-export const spreadsheetApiUrl = readable('https://script.google.com/macros/s/AKfycbwI_PjKnk_epE3ZtIudy4yeZFLbprvxpbplT9Y_6X6q0z5YVOXc95eGAIDBjcqwG_lHrQ/exec');
+export const spreadsheetApiUrl = readable('https://script.google.com/macros/s/AKfycbxfQ1Zdb80kOlQPmPXygbw93HbP_k4lOdaYOLh5OU3Gvuxry1uDVXSJFEdqaC33rN8H8A/exec');
